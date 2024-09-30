56cbe7c900fd27467ff2296b073c3fd06092d0df
Aryan Naraghi <aryan.naraghi@gmail.com>
2024-09-29 21:52:23 -0700

    Adds Clarity

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { clarity } from "react-microsoft-clarity";
 import { App } from "./App";
+
+clarity.init("ob552iy56o");
+clarity.consent();
+clarity.upgrade("all");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
