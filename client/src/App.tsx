d5db8c064419f61346d138de071759e4dd0b9af4
Aryan Naraghi <aryan.naraghi@gmail.com>
2024-09-30 20:18:20 -0700

    Removes unused dependencies.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { Board } from "./Board";
 import { Fen } from "./Fen";
 
 export const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
+    <>
       <Board />
-
       <Fen />
-    </DndProvider>
+    </>
   );
 };
