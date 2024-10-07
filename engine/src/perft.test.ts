02dd284402a43200c388f8a4ec3705ca7792e262
Aryan Naraghi <aryan.naraghi@gmail.com>
2024-10-06 19:24:19 -0700

    Adds another passing perft test.

--- engine/src/perft.test.ts
+++ engine/src/perft.test.ts
@@ -55,4 +55,13 @@ describe("perft", () => {
     );
     assert.deepEqual(actualStates, expectedStates);
   });
+
+  it("position 6", () => {
+    const expectedStates: number[] = [46];
+    const actualStates = runPerftTest(
+      expectedStates.length,
+      "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
+    );
+    assert.deepEqual(actualStates, expectedStates);
+  });
 });
