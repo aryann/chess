a6606f22991f1876e1520dfa08039ea32ed3991f
Aryan Naraghi <aryan.naraghi@gmail.com>
2024-11-10 19:08:50 -0800

    Reenables the pin unit tests now that the logic works.

--- engine/src/pins.test.ts
+++ engine/src/pins.test.ts
@@ -10,43 +10,43 @@ describe("pins", () => {
     assert.deepEqual(pins(board, "e1"), new Map<TSquare, TAllowedMoves>([]));
   });
 
-  it.skip("adjacent rooks", () => {
+  it("adjacent rooks", () => {
     const board = new BoardState("8/3r4/3R4/3K4/3R4/3r4/8/8 w KQkq - 0 1");
 
     assert.deepEqual(
       pins(board, "d5"),
       new Map<TSquare, TAllowedMoves>([
-        ["d4", []],
-        ["d6", []],
+        ["d4", ["d3"]],
+        ["d6", ["d7"]],
       ])
     );
   });
 
-  it.skip("far enemy rooks", () => {
+  it("far enemy rooks", () => {
     const board = new BoardState("3r4/8/3R4/3K4/3R4/8/8/3r4 w KQkq - 0 1");
 
     assert.deepEqual(
       pins(board, "d5"),
       new Map<TSquare, TAllowedMoves>([
-        ["d4", []],
-        ["d6", []],
+        ["d4", ["d3", "d2", "d1"]],
+        ["d6", ["d7", "d8"]],
       ])
     );
   });
 
-  it.skip("far friendly rooks", () => {
+  it("far friendly rooks", () => {
     const board = new BoardState("3r4/3R4/8/3K4/8/8/3R4/3r4 w KQkq - 0 1");
 
     assert.deepEqual(
       pins(board, "d5"),
       new Map<TSquare, TAllowedMoves>([
-        ["d2", ["d4", "d3"]],
-        ["d7", ["d6"]],
+        ["d2", ["d4", "d3", "d1"]],
+        ["d7", ["d6", "d8"]],
       ])
     );
   });
 
-  it.skip("adjacent bishops", () => {
+  it("adjacent rooks protecting against bishops", () => {
     const board = new BoardState("8/1b6/2R5/3K4/4R3/5b2/8/8 w KQkq - 0 1");
 
     assert.deepEqual(
@@ -67,12 +67,14 @@ describe("pins", () => {
     );
   });
 
-  it.skip("far friendly bishop with bishop pin", () => {
+  it("far friendly bishop with bishop pin", () => {
     const board = new BoardState("K7/8/8/8/8/8/6B1/7b w KQkq - 0 1");
 
     assert.deepEqual(
       pins(board, "a8"),
-      new Map<TSquare, TAllowedMoves>([["g2", ["b7", "c6", "d5", "e4", "f3"]]])
+      new Map<TSquare, TAllowedMoves>([
+        ["g2", ["b7", "c6", "d5", "e4", "f3", "h1"]],
+      ])
     );
   });
 
@@ -85,7 +87,7 @@ describe("pins", () => {
     );
   });
 
-  it.skip("adjacent queens", () => {
+  it("adjacent queens", () => {
     const board = new BoardState("8/3q4/3R4/3K4/3R4/3q4/8/8 w KQkq - 0 1");
 
     assert.deepEqual(
@@ -137,15 +139,9 @@ describe("pins", () => {
     );
   });
 
-  it.skip("multiple defenders", () => {
+  it("multiple defenders", () => {
     const board = new BoardState("3r4/3P4/3R4/3K4/3R4/3P4/8/3r4 w KQkq - 0 1");
 
-    assert.deepEqual(
-      pins(board, "d5"),
-      new Map<TSquare, TAllowedMoves>([
-        ["d4", []],
-        ["d6", []],
-      ])
-    );
+    assert.deepEqual(pins(board, "d5"), new Map<TSquare, TAllowedMoves>([]));
   });
 });
